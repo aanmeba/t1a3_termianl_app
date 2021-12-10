c9d21de36aa7fb17b0390d8ca5691f2b099cb451
aanmeba <aanmeba@gmail.com>
2021-12-10 18:30:10 +1100

    implements tty-prompt

--- src/main.rb
+++ src/main.rb
@@ -45,4 +45,26 @@ while bill_validation
    
 end
 
-puts "Choose how you are going to split the bill. R) Random E) Equal"
+choices = [
+        {name: 'I\'m feeling lucky!', value: 1},
+        {name: 'equally', value: 2},
+        {name: 'Exit', value: 3}
+      ]
+
+user_input = prompt.select("Choose how you are going to split the bill.", choices)
+
+case user_input
+when 1
+    y_n_input = prompt.yes?("Would you like to set a maximum amount for each person?") do |q|
+        q.required true
+        q.modify   :down
+    end
+
+    # this going to be into hell....
+    # call the methods...
+    
+when 2
+    puts "Alright, let's split the bill equally..."
+when 3
+    puts "Exiting application..."
+end
